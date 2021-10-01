20198707b544618bf68a4657b7e951a4a8385709
MYR <loic.themyr@gmail.com>
2021-10-01 11:26:05 +0200

    swin unnet - debug

--- mmseg/models/utils/swin_unet_utils.py
+++ mmseg/models/utils/swin_unet_utils.py
@@ -571,8 +571,8 @@ class BasicLayer_up(nn.Module):
         # if self.upsample is not None:
         #     x = self.upsample(x)
         if self.upsample is not None:
-            x_down = self.upsample(x, H, W)
-            Wh, Ww = (H) * 2, (W) * 2
+            x_down, Wh, Ww = self.upsample(x, H, W)
+            # Wh, Ww = (H) * 2, (W) * 2
             print("#### blu 0", Wh, Ww, x_down.shape)
             return x_down, Wh, Ww
         else:
