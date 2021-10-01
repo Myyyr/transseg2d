d151b358bc14f85350e643bdb220e413d7cadbc7
MYR <loic.themyr@gmail.com>
2021-10-01 10:43:03 +0200

    swin unnet - debug

--- mmseg/models/decode_heads/swin_unet_decoder.py
+++ mmseg/models/decode_heads/swin_unet_decoder.py
@@ -170,7 +170,7 @@ class SwinUNetDecoder(BaseDecodeHead):
     #Dencoder and Skip connection
     def forward_up_features(self, x, x_downsample):
     	print("### ok 0", x.shape)
-    	ewit(0)
+    	exit(0)
         Wh, Ww = x.size(2), x.size(3)
         for inx, layer_up in enumerate(self.layers_up):
             if inx == 0:
