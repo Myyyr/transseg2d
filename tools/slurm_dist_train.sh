d5c739e2af175776e9fd79294f0278cf4342da3b
Myyyr <loic.themyr@gmail.com>
2021-10-07 11:36:13 +0200

    slurm

--- tools/slurm_dist_train.sh
+++ tools/slurm_dist_train.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=pytorch_stl10     # job name
+#SBATCH --job-name=swinade     # job name
 #SBATCH --ntasks=8                   # number of MP tasks
 #SBATCH --ntasks-per-node=4          # number of MPI tasks per node
 #SBATCH --gres=gpu:4                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=00:35:00              # maximum execution time (HH:MM:SS)
-#SBATCH --output=pytorch_stl10%j.out # output file name
-#SBATCH --error=pytorch_stl10%j.err  # error file name
+#SBATCH --output=swinade%j.out # output file name
+#SBATCH --error=swinade%j.err  # error file name
 
 set -x
+
+cd $WORK/transseg2d
 
 module purge
 module load cuda/10.1.2
@@ -18,6 +20,6 @@ CONFIG="configs/swin/upernet_swin_base_patch4_window7_512x512_160k_ade20k.py"
 GPUS=8
 PORT=${PORT:-29500}
 
-PYTHONPATH="$(dirname $CONFIG)/..":$PYTHONPATH \
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 srun python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
