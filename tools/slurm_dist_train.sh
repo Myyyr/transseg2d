a7c7dcdbd3f38c53c59b1349ac404261900e0b8f
Myyyr <loic.themyr@gmail.com>
2021-10-07 11:51:16 +0200

    slurm

--- tools/slurm_dist_train.sh
+++ tools/slurm_dist_train.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=swinade     # job name
-#SBATCH --ntasks=8                   # number of MP tasks
-#SBATCH --ntasks-per-node=4          # number of MPI tasks per node
-#SBATCH --gres=gpu:4                 # number of GPUs per node
+#SBATCH --ntasks=1                   # number of MP tasks
+#SBATCH --ntasks-per-node=1          # number of MPI tasks per node
+#SBATCH --gres=gpu:1                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
-#SBATCH --time=00:35:00              # maximum execution time (HH:MM:SS)
+#SBATCH --time=00:10:00              # maximum execution time (HH:MM:SS)
+#SBATCH --qos=qos_gpu-t3
 #SBATCH --output=swinade%j.out # output file name
 #SBATCH --error=swinade%j.err  # error file name
 
@@ -17,7 +18,7 @@ module purge
 module load cuda/10.1.2
 
 CONFIG="configs/swin/upernet_swin_base_patch4_window7_512x512_160k_ade20k.py"
-GPUS=8
+GPUS=1
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
