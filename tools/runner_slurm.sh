93ec2940216c882def66581c06609b258e05df08
Myyyr <loic.themyr@gmail.com>
2021-10-07 17:25:18 +0200

    slurm - swin

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=swin_tiny_ade     # job name
-#SBATCH --ntasks=3                  # number of MP tasks
-#SBATCH --ntasks-per-node=3          # number of MPI tasks per node
-#SBATCH --gres=gpu:3                 # number of GPUs per node
-#SBATCH --cpus-per-task=10           # number of cores per tasks
+#SBATCH --ntasks=8                   # nombre total de tache MPI (= nombre total de GPU)
+#SBATCH --ntasks-per-node=4          # nombre de tache MPI par noeud (= nombre de GPU par noeud)
+#SBATCH --gres=gpu:4                 # nombre de GPU par nœud (max 8 avec gpu_p2)
+#SBATCH --cpus-per-task=10           # nombre de coeurs CPU par tache (un quart du noeud ici)
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=00:10:00              # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t3
@@ -20,9 +20,9 @@ module load cuda/10.1.2
 
 
 CONFIG="configs/orininal_swin/upernet_swin_tiny_patch4_window7_512x512_160k_ade20k.py"
-GPUS=3
+GPUS=8
 PORT=${PORT:-29500}
 
 
-srun python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+srun /gpfslocalsup/pub/idrtools/bind_gpu.sh python -u -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     tools/train.py $CONFIG --launcher pytorch ${@:3}
