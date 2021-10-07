2ffe2ba674e673ebb6c3ebc723ff427992feb555
Myyyr <loic.themyr@gmail.com>
2021-10-07 14:35:56 +0200

    slurm - swin

--- tools/slurm_dist_train.sh
+++ tools/slurm_dist_train.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=swinade     # job name
-#SBATCH --ntasks=1                   # number of MP tasks
-#SBATCH --ntasks-per-node=1          # number of MPI tasks per node
-#SBATCH --gres=gpu:1                 # number of GPUs per node
+#SBATCH --job-name=swin_tiny_ade     # job name
+#SBATCH --ntasks=8                   # number of MP tasks
+#SBATCH --ntasks-per-node=4          # number of MPI tasks per node
+#SBATCH --gres=gpu:4                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=00:10:00              # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t3
-#SBATCH --output=swinade%j.out # output file name
-#SBATCH --error=swinade%j.err  # error file name
+#SBATCH --output=swin_tiny_ade%j.out # output file name
+#SBATCH --error=swin_tiny_ade%j.err  # error file name
 
 set -x
 
@@ -17,8 +17,8 @@ cd $WORK/transseg2d
 module purge
 module load cuda/10.1.2
 
-CONFIG="configs/swin/upernet_swin_base_patch4_window7_512x512_160k_ade20k.py"
-GPUS=1
+CONFIG="configs/orininal_swin/upernet_swin_tiny_patch4_window7_512x512_160k_ade20k.py"
+GPUS=8
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
