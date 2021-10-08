4e12c81c49c2c5970d9b7025ee1a9317d1c0224d
MYR <loic.themyr@gmail.com>
2021-10-08 19:09:05 +0200

    swin unet   - debug

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=swin_tiny_ade_pt     # job name
+#SBATCH --job-name=swinunet_tiny_ade_pt     # job name
 #SBATCH --ntasks=8                  # number of MP tasks
 #SBATCH --ntasks-per-node=4          # number of MPI tasks per node
 #SBATCH --gres=gpu:4                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=00:10:00              # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t3
-#SBATCH --output=logs/swin_tiny_ade_pt%j.out # output file name
-#SBATCH --error=logs/swin_tiny_ade_pt%j.err  # error file name
+#SBATCH --output=logs/swinunet_tiny_ade_pt%j.out # output file name
+#SBATCH --error=logs/swinunet_tiny_ade_pt%j.err  # error file name
 
 set -x
 
