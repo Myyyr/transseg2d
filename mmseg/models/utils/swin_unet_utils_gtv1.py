0c849d1e37248da9f408166df68e6eaab533e375
MYR <loic.themyr@gmail.com>
2021-10-08 15:50:05 +0200

    swin   - debug

--- mmseg/models/utils/swin_unet_utils_gtv1.py
+++ mmseg/models/utils/swin_unet_utils_gtv1.py
@@ -138,7 +138,8 @@ class WindowAttention(nn.Module):
 
 
         qkv = self.qkv(x).reshape(B_, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
-        # print("#------> qkv",qkv.shape)
+        print("#------> qkv",qkv.shape)
+        exit(0)
         
         q, k, v = qkv[0], qkv[1], qkv[2]  # make torchscript happy (cannot use tensor as tuple)
 
