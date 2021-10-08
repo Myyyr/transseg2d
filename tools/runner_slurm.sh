1701fe746330098528d16acfd871b04843b52d16
MYR <loic.themyr@gmail.com>
2021-10-08 18:30:02 +0200

    swin unet   - debug

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -19,6 +19,7 @@ module load cuda/10.1.2
 
 
 
-CONFIG="configs/orininal_swin/upernet_swin_tiny_pt_patch4_window7_512x512_160k_ade20k.py"
+# CONFIG="configs/orininal_swin/upernet_swin_tiny_pt_patch4_window7_512x512_160k_ade20k.py"
+CONFIG="configs/swinunet/swinunet_tiny_patch4_window7_512x512_160k_ade20k.py"
 
 srun /gpfslocalsup/pub/idrtools/bind_gpu.sh python -u tools/train.py $CONFIG --options model.pretrained="pretrained_models/swin_tiny_patch4_window7_224.pth" --launcher="slurm" ${@:3}
