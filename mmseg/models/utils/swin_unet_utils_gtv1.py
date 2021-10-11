b1d38df6e7594e0eeac0c141dd15ba18a87da3e7
MYR <loic.themyr@gmail.com>
2021-10-11 10:07:57 +0200

    swin unet   - debug

--- mmseg/models/utils/swin_unet_utils_gtv1.py
+++ mmseg/models/utils/swin_unet_utils_gtv1.py
@@ -173,7 +173,8 @@ class WindowAttention(nn.Module):
 
         if mask is not None:
             nW = mask.shape[0]
-            print("-----> mask",mask.shape, mask.min(), mask.max())
+            print("-----> mask",mask.unsqueeze(1).unsqueeze(0).shape, mask.min(), mask.max())
+            exit(0)
             attn = attn.view(B_ // nW, nW, self.num_heads, N, N) + mask.unsqueeze(1).unsqueeze(0)
             attn = attn.view(-1, self.num_heads, N, N)
             attn = self.softmax(attn)
