60510085c984bf4dac5538320a114cd8552f938f
MYR <loic.themyr@gmail.com>
2021-10-11 09:59:50 +0200

    swin unet   - debug

--- mmseg/models/utils/swin_unet_utils_gtv1.py
+++ mmseg/models/utils/swin_unet_utils_gtv1.py
@@ -294,9 +294,9 @@ class SwinTransformerBlock(nn.Module):
         x_windows = x_windows.view(-1, self.window_size * self.window_size, C)  # nW*B, window_size*window_size, C
 
         # W-MSA/SW-MSA
-        print("----> WE are here !")
+        # print("----> WE are here !")
         attn_windows = self.attn(x_windows, mask=attn_mask)  # nW*B, window_size*window_size, C
-        print("----> and now here !")
+        # print("----> and now here !")
 
         # merge windows
         attn_windows = attn_windows.view(-1, self.window_size, self.window_size, C)
@@ -315,10 +315,10 @@ class SwinTransformerBlock(nn.Module):
 
         # FFN
         x = shortcut + self.drop_path(x)
-        print("----> before mlp", x.shape)
+        # print("----> before mlp", x.shape)
         x = x + self.drop_path(self.mlp(self.norm2(x)))
-        print("----> after mlp")
-        exit(0)
+        # print("----> after mlp")
+        # exit(0)
 
         return x
 
