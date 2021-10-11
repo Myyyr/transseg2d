60d0e0065e321126b77cb084c210786845b8da84
MYR <loic.themyr@gmail.com>
2021-10-11 10:11:21 +0200

    swin unet   - debug

--- mmseg/models/utils/swin_unet_utils_gtv1.py
+++ mmseg/models/utils/swin_unet_utils_gtv1.py
@@ -294,7 +294,9 @@ class SwinTransformerBlock(nn.Module):
         # partition windows
         x_windows = window_partition(shifted_x, self.window_size)  # nW*B, window_size, window_size, C
         x_windows = x_windows.view(-1, self.window_size * self.window_size, C)  # nW*B, window_size*window_size, C
-
+        print("=====> xwin",x_windows.shape, x_windows.min(), x.windows.max())
+        exit(0)
+        x_windows = torch.cat
         # W-MSA/SW-MSA
         # print("----> WE are here !")
         attn_windows = self.attn(x_windows, mask=attn_mask)  # nW*B, window_size*window_size, C
