772164e3cd31ce99831e9688e5a7d0efd7f3ccfd
Myyyr <loic.themyr@gmail.com>
2021-10-12 14:32:54 +0200

    swin - slurm

--- configs/swinunetgtvdbg/swinunetgtvdbg_tiny_patch4_window7_512x512_160k_ade20k.py
+++ configs/swinunetgtvdbg/swinunetgtvdbg_tiny_patch4_window7_512x512_160k_ade20k.py
@@ -13,7 +13,7 @@ model = dict(
         patch_norm=True,
         use_checkpoint=False,
         num_classes=150, 
-        gt_num=10
+        gt_num=1
     ),
     decode_head=dict(
         embed_dim=96,
@@ -25,7 +25,7 @@ model = dict(
         patch_norm=True,
         use_checkpoint=False,
         num_classes=150, 
-        gt_num=10
+        gt_num=1
     )
     #,
     # auxiliary_head=dict(
