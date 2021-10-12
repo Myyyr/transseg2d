a0310505fcfed62402f73b6b576fdf46da690131
Myyyr <loic.themyr@gmail.com>
2021-10-12 14:04:12 +0200

    swin - slurm

--- configs/_base_/datasets/ade20kdbg.py
+++ configs/_base_/datasets/ade20kdbg.py
@@ -3,11 +3,11 @@ dataset_type = 'ADE20KDataset'
 data_root = 'data/ade/ADEChallengeData2016'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (256, 256)
+crop_size = (128, 128)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', reduce_zero_label=True),
-    dict(type='Resize', img_scale=(1024, 256), ratio_range=(0.5, 2.0)),
+    dict(type='Resize', img_scale=(512, 128), ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
@@ -20,7 +20,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1024, 256),
+        img_scale=(512, 128),
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
