81b5494e5ef7a2df3b8418c7cfbbef1e95f17e00
Myyyr <loic.themyr@gmail.com>
2021-10-14 15:08:39 +0200

    swin - slurm

--- mmseg/models/utils/swin_unet_v2_utils.py
+++ mmseg/models/utils/swin_unet_v2_utils.py
@@ -694,7 +694,7 @@ class SwinTransformerSys(nn.Module):
 
         # split image into non-overlapping patches
         self.patch_embed = PatchEmbed(
-            img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim,
+            img_size=pretrain_img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim,
             norm_layer=norm_layer if self.patch_norm else None)
         num_patches = self.patch_embed.num_patches
         patches_resolution = self.patch_embed.patches_resolution
