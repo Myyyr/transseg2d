d3a535f6d3d4661460890887db4c4f89c0c49200
Myyyr <loic.themyr@gmail.com>
2021-10-19 10:55:26 +0200

    swin - slurm

--- mmseg/models/backbones/__init__.py
+++ mmseg/models/backbones/__init__.py
@@ -12,6 +12,7 @@ from .swin_unet_encoder import SwinUNetEncoder
 from .swin_unet_v2 import SwinUNetV2
 from .swin_unet_v2_gtv3 import SwinUNetV2GTV3
 from .swin_unet_v2_gtv4 import SwinUNetV2GTV4
+from .swin_unet_v2_gtv4_dbg import SwinUNetV2GTV4DBG
 from .no_swin_unet_v2 import NoSwinUNetV2
 from .swin_unet_encoder_gtv1 import SwinUNetEncoderGTv1
 from .swin_unet_encoder_gtv2 import SwinUNetEncoderGTv2
@@ -23,5 +24,5 @@ __all__ = [
     'ResNet', 'ResNetV1c', 'ResNetV1d', 'ResNeXt', 'HRNet', 'FastSCNN',
     'ResNeSt', 'MobileNetV2', 'UNet', 'CGNet', 'MobileNetV3', 'SwinTransformer', 
     'SwinUNetEncoder', 'SwinUNetEncoderGTv1', 'SwinUNetEncoderGTv2', 'SwinUNetEncoderGTvdbg', 'SwinUNetEncoderGTvdbg2'
-    , 'SwinUNetEncoderGTvdbg3', 'SwinUNetV2', 'NoSwinUNetV2', 'SwinUNetV2GTV3', 'SwinUNetV2GTV4'
+    , 'SwinUNetEncoderGTvdbg3', 'SwinUNetV2', 'NoSwinUNetV2', 'SwinUNetV2GTV3', 'SwinUNetV2GTV4', 'SwinUNetV2GTV4DBG'
 ]
