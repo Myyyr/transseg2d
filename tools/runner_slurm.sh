792a18e1839fa8d406f3c42cb0111e2ef4586070
MYR <loic.themyr@gmail.com>
2021-10-20 15:05:40 +0200

    new config - swin unet gt

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:4                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
-#SBATCH --time=20:00:00              # maximum execution time (HH:MM:SS)
+#SBATCH --time=15:00:00              # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t3
 #SBATCH --output=logs/su24g5_tiny_city%j.out # output file name
 #SBATCH --error=logs/su24g5_tiny_city%j.err  # error file name
