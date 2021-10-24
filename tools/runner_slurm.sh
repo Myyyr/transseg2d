6d88c4e4cb13129b80fe52f0be16c05b01083384
MYR <loic.themyr@gmail.com>
2021-10-24 23:40:30 +0200

    debug - swin unet gt

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=s2saade     # job name
+#SBATCH --job-name=s2baade     # job name
 #SBATCH --ntasks=8                  # number of MP tasks
 #SBATCH --ntasks-per-node=4          # number of MPI tasks per node
 #SBATCH --gres=gpu:4                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=30:00:00              # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t4
-#SBATCH --output=logs/s2saade%j.out # output file name
-#SBATCH --error=logs/s2saade%j.err  # error file name
+#SBATCH --output=logs/s2baade%j.out # output file name
+#SBATCH --error=logs/s2baade%j.err  # error file name
 
 set -x
 
@@ -45,16 +45,16 @@ module load cuda/10.1.2
 # CONFIG="configs/swinunetv2gtv4/swinunetv2gtv4_g5_tiny_patch4_window7_769x769_160k_cityscapes.py"
 # CONFIG="configs/swinunetv2gtv3/swinunetv2gtv3_g5_tiny_patch4_window7_769x769_160k_cityscapes.py"
 
-CONFIG="configs/swinunetv2/swinunetv2_small_patch4_window7_512x512_160k_ade20k.py"
-# CONFIG="configs/swinunetv2/swinunetv2_base_patch4_window7_512x512_160k_ade20k.py"
+# CONFIG="configs/swinunetv2/swinunetv2_small_patch4_window7_512x512_160k_ade20k.py"
+CONFIG="configs/swinunetv2/swinunetv2_base_patch4_window7_512x512_160k_ade20k.py"
 
 
 
 
 
 # PRET="pretrained_models/swin_tiny_patch4_window7_224.pth"
-PRET="pretrained_models/swin_small_patch4_window7_224.pth"
-# PRET="pretrained_models/swin_base_patch4_window7_224.pth"
+# PRET="pretrained_models/swin_small_patch4_window7_224.pth"
+PRET="pretrained_models/swin_base_patch4_window7_224.pth"
 # PRET="pretrained_models/swin_base_patch4_window7_224_22k.pth"
 ## PRET="pretrained_models/swin_base_patch4_window12_384_22k.pth"
 ## PRET="pretrained_models/swin_base_patch4_window12_384.pth"
