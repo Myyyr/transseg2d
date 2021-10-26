a71f05002cf659a0eaa7edeb31d9589d1970920d
MYR <loic.themyr@gmail.com>
2021-10-26 17:11:45 +0200

    debug - swin unet gt - self attn merg

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=00:05:00              # maximum execution time (HH:MM:SS)
-#SBATCH --qos=qos_gpu-t3
+#SBATCH --qos=qos_gpu-dev
 #SBATCH --output=logs/s28g10tade%j.out # output file name
 #SBATCH --error=logs/s28g10tade%j.err  # error file name
 
@@ -16,6 +16,7 @@ set -x
 cd $WORK/transseg2d
 module purge
 module load cuda/10.1.2
+module load python/3.7.10
 
 
 # RESUME="work_dirs/swinunet_tiny_patch4_window7_512x512_160k_ade20k/iter_128000.pth"
