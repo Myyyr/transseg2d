55461f792a4f540309b5cad403fe0407526cf271
Myyyr <loic.themyr@gmail.com>
2021-10-27 09:43:20 +0200

    swin - slurm

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=s28g5tade     # job name
+#SBATCH --job-name=s28g10tade     # job name
 #SBATCH --ntasks=8                  # number of MP tasks
 #SBATCH --ntasks-per-node=4          # number of MPI tasks per node
 #SBATCH --gres=gpu:4                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=18:00:00              # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t3
-#SBATCH --output=logs/s28g5tade%j.out # output file name
-#SBATCH --error=logs/s28g5tade%j.err  # error file name
+#SBATCH --output=logs/s28g10tade%j.out # output file name
+#SBATCH --error=logs/s28g10tade%j.err  # error file name
 
 set -x
 
