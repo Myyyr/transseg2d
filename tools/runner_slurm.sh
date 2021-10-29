b11df65e7d0605b4cf6f49c0abc00e376fede57d
MYR <loic.themyr@gmail.com>
2021-10-29 21:32:43 +0200

    debug - swin unet gt - self attn merg

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=s27g1tade     # job name
+#SBATCH --job-name=s28g1tadec     # job name
 #SBATCH --ntasks=8                  # number of MP tasks
 #SBATCH --ntasks-per-node=4          # number of MPI tasks per node
 #SBATCH --gres=gpu:4                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=16:05:00              # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t3
-#SBATCH --output=logs/s27g1tade%j.out # output file name
-#SBATCH --error=logs/s27g1tade%j.err  # error file name
+#SBATCH --output=logs/s28g1tadec%j.out # output file name
+#SBATCH --error=logs/s28g1tadec%j.err  # error file name
 
 set -x
 
@@ -25,7 +25,7 @@ module load python/3.7.10
 # RESUME="work_dirs/swinunetv2gtv8_g10_tiny_patch4_window7_512x512_160k_ade20k/latest.pth"
 # RESUME="work_dirs/swinunetv2gtv8_g1_tiny_patch4_window7_512x512_160k_ade20k/latest.pth"
 # RESUME="work_dirs/swinunetv2gtv8_g5_tiny_patch4_window7_512x512_160k_ade20k/latest.pth"
-# RESUME="work_dirs/swinunetv2gtv8_g1_tiny_patch4_window7_512x512_160k_ade20k_good/latest.pth"
+RESUME="work_dirs/swinunetv2gtv8_g1_tiny_patch4_window7_512x512_160k_ade20k_good/latest.pth"
 
 
 
@@ -80,8 +80,8 @@ module load python/3.7.10
 
 # --------------------------------------------- 
 # CONFIG="configs/swinunetv2/swinunetv2_tiny_patch4_window7_512x512_160k_ade20k_good.py"
-CONFIG="configs/swinunetv2gtv7/swinunetv2gtv7_g1_tiny_patch4_window7_512x512_160k_ade20k_good.py"
-# CONFIG="configs/swinunetv2gtv8/swinunetv2gtv8_g1_tiny_patch4_window7_512x512_160k_ade20k_good.py"
+# CONFIG="configs/swinunetv2gtv7/swinunetv2gtv7_g1_tiny_patch4_window7_512x512_160k_ade20k_good.py"
+CONFIG="configs/swinunetv2gtv8/swinunetv2gtv8_g1_tiny_patch4_window7_512x512_160k_ade20k_good.py"
 
 
 
