10af6a7b7268a024da5abc7270c0611e6e1cb28f
MYR <loic.themyr@gmail.com>
2021-10-31 18:17:46 +0100

    debug - swin unet gt - self attn merg

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=s2tadegc     # job name
+#SBATCH --job-name=s2badegc     # job name
 #SBATCH --ntasks=8                  # number of MP tasks
 #SBATCH --ntasks-per-node=4          # number of MPI tasks per node
 #SBATCH --gres=gpu:4                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=16:05:00              # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t3
-#SBATCH --output=logs/s2tadegc%j.out # output file name
-#SBATCH --error=logs/s2tadegc%j.err  # error file name
+#SBATCH --output=logs/s2badegc%j.out # output file name
+#SBATCH --error=logs/s2badegc%j.err  # error file name
 
 set -x
 
