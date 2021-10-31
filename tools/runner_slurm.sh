06017adf95ad377483ad185288a75ba0c77f09d6
MYR <loic.themyr@gmail.com>
2021-10-31 18:20:23 +0100

    debug - swin unet gt - self attn merg

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=suptadegc     # job name
+#SBATCH --job-name=s28g10tadegc     # job name
 #SBATCH --ntasks=8                  # number of MP tasks
 #SBATCH --ntasks-per-node=4          # number of MPI tasks per node
 #SBATCH --gres=gpu:4                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=16:05:00              # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t3
-#SBATCH --output=logs/suptadegc%j.out # output file name
-#SBATCH --error=logs/suptadegc%j.err  # error file name
+#SBATCH --output=logs/s28g10tadegc%j.out # output file name
+#SBATCH --error=logs/s28g10tadegc%j.err  # error file name
 
 set -x
 
@@ -89,11 +89,11 @@ module load python/3.7.10
 # CONFIG="configs/swinunetv2/swinunetv2_base_patch4_window7_512x512_160k_ade20k_good.py"
 # CONFIG="configs/swinunetv2/swinunetv2_tiny_patch4_window7_769x769_160k_cityscapes_good.py"
 
-CONFIG="configs/orininal_swin/upernet_swin_tiny_patch4_window7_512x512_160k_ade20k_good.py"
+# CONFIG="configs/orininal_swin/upernet_swin_tiny_patch4_window7_512x512_160k_ade20k_good.py"
 
 ## CONFIG="configs/swinunetv2gtv8/swinunetv2gtv8_g1_tiny_patch4_window7_512x512_160k_ade20k_good.py"
 ## CONFIG="configs/swinunetv2gtv8/swinunetv2gtv8_g5_tiny_patch4_window7_512x512_160k_ade20k_good.py"
-# CONFIG="configs/swinunetv2gtv8/swinunetv2gtv8_g10_tiny_patch4_window7_512x512_160k_ade20k_good.py"
+CONFIG="configs/swinunetv2gtv8/swinunetv2gtv8_g10_tiny_patch4_window7_512x512_160k_ade20k_good.py"
 
 # CONFIG="configs/swinunetv2gtv8/swinunetv2gtv8_g1_small_patch4_window7_512x512_160k_ade20k_good.py"
 # CONFIG="configs/swinunetv2gtv8/swinunetv2gtv8_g1_base_patch4_window7_512x512_160k_ade20k_good.py"
@@ -118,7 +118,8 @@ CONFIG="configs/orininal_swin/upernet_swin_tiny_patch4_window7_512x512_160k_ade2
 # RESUME="work_dirs/swinunetv2_small_patch4_window7_512x512_160k_ade20k_good/latest.pth"
 # RESUME="work_dirs/swinunetv2_base_patch4_window7_512x512_160k_ade20k_good/latest.pth"
 # RESUME="work_dirs/swinunetv2_tiny_patch4_window7_769x769_160k_cityscapes_good/latest.pth"
-RESUME="work_dirs/upernet_swin_tiny_patch4_window7_512x512_160k_ade20k_good/latest.pth"
+# RESUME="work_dirs/upernet_swin_tiny_patch4_window7_512x512_160k_ade20k_good/latest.pth"
+RESUME="work_dirs/swinunetv2gtv8_g10_tiny_patch4_window7_512x512_160k_ade20k_good/latest.pth"
 
 
 
