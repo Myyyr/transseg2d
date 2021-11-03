c99245841d634b45d54bd53e24c586945813d693
Myyyr <loic.themyr@gmail.com>
2021-11-03 11:16:48 +0100

    swin - slurm

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=s28g5tadegz     # job name
+#SBATCH --job-name=s28g10tadegz     # job name
 #SBATCH --ntasks=8                  # number of MP tasks
 #SBATCH --ntasks-per-node=4          # number of MPI tasks per node
 #SBATCH --gres=gpu:4                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=16:05:00              # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t3
-#SBATCH --output=logs/s28g5tadegz%j.out # output file name
-#SBATCH --error=logs/s28g5tadegz%j.err  # error file name
+#SBATCH --output=logs/s28g10tadegz%j.out # output file name
+#SBATCH --error=logs/s28g10tadegz%j.err  # error file name
 
 set -x
 
@@ -125,8 +125,8 @@ module load python/3.7.10
 # CONFIG="configs/orininal_swin/zupernet_swin_tiny_patch4_window7_512x512_160k_ade20k_good.py"
 
 # CONFIG="configs/swinunetv2gtv8/zswinunetv2gtv8_g1_tiny_patch4_window7_512x512_160k_ade20k_good.py"
-CONFIG="configs/swinunetv2gtv8/zswinunetv2gtv8_g5_tiny_patch4_window7_512x512_160k_ade20k_good.py"
-# CONFIG="configs/swinunetv2gtv8/zswinunetv2gtv8_g10_tiny_patch4_window7_512x512_160k_ade20k_good.py"
+# CONFIG="configs/swinunetv2gtv8/zswinunetv2gtv8_g5_tiny_patch4_window7_512x512_160k_ade20k_good.py"
+CONFIG="configs/swinunetv2gtv8/zswinunetv2gtv8_g10_tiny_patch4_window7_512x512_160k_ade20k_good.py"
 
 
 
