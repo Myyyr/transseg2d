f8f3e72ba9484345db715ff50edb8f528d76f044
MYR <loic.themyr@gmail.com>
2021-11-04 18:15:20 +0100

    debug - swin unet gt - self attn merg

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -5,11 +5,10 @@
 #SBATCH --gres=gpu:4                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
-#SBATCH --time=10:05:00              # maximum execution time (HH:MM:SS)
+#SBATCH --time=16:05:00              # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t3
 #SBATCH --output=logs/s28g10tcitgzc%j.out # output file name
 #SBATCH --error=logs/s28g10tcitgzc%j.err  # error file name
-#SBATCH -C v100-32g
 
 set -x
 
