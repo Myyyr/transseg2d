0698aa27b605e8a3d1202084fa19215e1d575e90
MYR <loic.themyr@gmail.com>
2021-11-10 16:13:37 +0100

    swin uper - configs

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=dbgsupbcitz     # job name
+#SBATCH --job-name=supbcitz     # job name
 #SBATCH --ntasks=8                  # number of MP tasks
 #SBATCH --ntasks-per-node=4          # number of MPI tasks per node
 #SBATCH --gres=gpu:4                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
-#SBATCH --time=00:05:00              # maximum execution time (HH:MM:SS)
-#SBATCH --qos=qos_gpu-dev
-#SBATCH --output=logs/dbgsupbcitz%j.out # output file name
-#SBATCH --error=logs/dbgsupbcitz%j.err  # error file name
+#SBATCH --time=19:05:00              # maximum execution time (HH:MM:SS)
+#SBATCH --qos=qos_gpu-t3
+#SBATCH --output=logs/supbcitz%j.out # output file name
+#SBATCH --error=logs/supbcitz%j.err  # error file name
+#SBATCH -C v100-32g
 
 set -x
 
