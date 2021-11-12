582b60303557e37e815173a2dc83acc4d40b5a5c
MYR <loic.themyr@gmail.com>
2021-11-12 21:18:42 +0100

    seg form

--- mmseg/models/backbones/mix_transformer_gt.py
+++ mmseg/models/backbones/mix_transformer_gt.py
@@ -149,7 +149,7 @@ class Attention(nn.Module):
         q = self.q(x_windows).reshape(B, N_, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
 
         if self.sr_ratio > 1:
-            x_ = x.permute(0, 2, 1).reshape(B, C, Hp, Wp)
+            x_ = x.permute(0, 2, 1).reshape(B, C, self.window_size, self.window_size)
             x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
             x_ = self.norm(x_)
             kv = self.kv(x_).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
