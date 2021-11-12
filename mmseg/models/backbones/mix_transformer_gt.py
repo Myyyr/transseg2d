5dd0828658bbf1f7e40cfe7363d599e89a55d04e
MYR <loic.themyr@gmail.com>
2021-11-12 21:24:27 +0100

    seg form

--- mmseg/models/backbones/mix_transformer_gt.py
+++ mmseg/models/backbones/mix_transformer_gt.py
@@ -187,7 +187,7 @@ class Block(nn.Module):
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
-        self.window_size=7
+        self.window_size=8
         self.apply(self._init_weights)
 
     def _init_weights(self, m):
