fdddee95c3fbc326cbcb03c79891020eaa729ab1
MYR <loic.themyr@gmail.com>
2021-11-13 15:59:55 +0100

    segformer no sr

--- mmseg/models/backbones/mix_transformer_gt.py
+++ mmseg/models/backbones/mix_transformer_gt.py
@@ -169,7 +169,11 @@ class Attention(nn.Module):
             self.sr = nn.Conv2d(dim, dim, kernel_size=sr_ratio, stride=sr_ratio)
             self.norm = nn.LayerNorm(dim)
 
+            self.proj = nn.Linear(dim, dim*sr_ratio)
+
         self.apply(self._init_weights)
+
+
 
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
@@ -211,13 +215,14 @@ class Attention(nn.Module):
 
         q = self.q(x_windows).reshape(B, N_, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
 
-        # if self.sr_ratio > 1:
-        #     x_ = x_windows[:,self.gt_num:,:].permute(0, 2, 1).reshape(B, C, self.window_size, self.window_size)
-        #     x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
-        #     x_ = torch.cat([gt, x_], dim=1)
-        #     x_ = self.norm(x_)
-        #     kv = self.kv(x_).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
-        # else:
+        if self.sr_ratio > 1:
+            x_ = self.proj(x_windows[:,self.gt_num:,:])
+            x_ = x_.permute(0, 2, 1).reshape(B, C, self.window_size*self.sr_ratio, self.window_size*self.sr_ratio)
+            x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
+            x_ = torch.cat([gt, x_], dim=1)
+            x_ = self.norm(x_)
+            kv = self.kv(x_).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        else:
         kv = self.kv(x_windows).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
         k, v = kv[0], kv[1]
 
