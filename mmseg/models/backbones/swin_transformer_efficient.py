4726a1d251f41af9ccdc4f2fa2073ede283641f3
MYR <loic.themyr@gmail.com>
2021-11-15 15:59:10 +0100

    sync

--- mmseg/models/backbones/swin_transformer_efficient.py
+++ mmseg/models/backbones/swin_transformer_efficient.py
@@ -157,7 +157,8 @@ class WindowAttention(nn.Module):
         relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1)].view(
             self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)  # Wh*Ww,Wh*Ww,nH
         relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
-        attn = attn + relative_position_bias.unsqueeze(0)
+        
+        attn = attn + nn.functional.interpolate(relative_position_bias.unsqueeze(0), size=(attn.shape[-2],attn.shape[-1]), mode='bilinear')
 
         if mask is not None:
             nW = mask.shape[0]
