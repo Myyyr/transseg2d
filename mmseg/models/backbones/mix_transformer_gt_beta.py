52b1504a41fa479547d04bd214bf02d2a6316cbb
MYR <loic.themyr@gmail.com>
2021-11-16 00:44:56 +0100

    sync

--- mmseg/models/backbones/mix_transformer_gt_beta.py
+++ mmseg/models/backbones/mix_transformer_gt_beta.py
@@ -156,7 +156,7 @@ class Attention(nn.Module):
             gt = repeat(gt, "g c -> b g c", b=B)# shape of (num_windows*B, G, C)
         gt_num = gt.shape[1]
         x = torch.cat([gt, x], dim=1)
-        B, N, C = x_windows.shape
+        B, N, C = x.shape
 
         q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
 
@@ -516,7 +516,7 @@ class mit_gt_beta_b4(MixVisionTransformerGTBeta):
         super(mit_gt_beta_b4, self).__init__(
             patch_size=4, embed_dims=[64, 128, 320, 512], num_heads=[1, 2, 5, 8], mlp_ratios=[4, 4, 4, 4],
             qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[3, 8, 27, 3], sr_ratios=[8, 4, 2, 1],
-            drop_rate=0.0, drop_path_rate=0.1)
+            drop_rate=0.0, drop_path_rate=0.1, gt_num=10)
 
 
 @BACKBONES.register_module()
