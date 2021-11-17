995f3c67b8afde94d37566d3bec72bc70729a1c7
MYR <loic.themyr@gmail.com>
2021-11-17 22:05:10 +0100

    sync

--- mmseg/models/backbones/mix_transformer_gt_gamma.py
+++ mmseg/models/backbones/mix_transformer_gt_gamma.py
@@ -46,9 +46,10 @@ class Attention(nn.Module):
         B, N_, C = x.shape
         gt_num = self.gt_num
 
-        if len(gt.shape) != 3:
-            gt = repeat(gt, "g c -> b g c", b=B)# shape of (num_windows*B, G, C)
-        x = torch.cat([gt, x], dim=1)
+        if gt_num != 0:
+            if len(gt.shape) != 3:
+                gt = repeat(gt, "g c -> b g c", b=B)# shape of (num_windows*B, G, C)
+            x = torch.cat([gt, x], dim=1)
         B, N, C = x.shape
 
         q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
