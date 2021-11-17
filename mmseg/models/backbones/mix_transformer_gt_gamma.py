ffa31f7c03d05372f1d41a3abe958991939a7d04
MYR <loic.themyr@gmail.com>
2021-11-17 18:34:29 +0100

    sync

--- mmseg/models/backbones/mix_transformer_gt_gamma.py
+++ mmseg/models/backbones/mix_transformer_gt_gamma.py
@@ -102,29 +102,26 @@ class SegFormerGTGamma(nn.Module):
         if isinstance(pretrained, str):
             self.mix.init_weights(pretrained)
 
+        depths=[3, 8, 27, 3]
+
         self.patch_embed1 = self.mix.patch_embed1
         self.patch_embed2 = self.mix.patch_embed2
         self.patch_embed3 = self.mix.patch_embed3 
         self.patch_embed4 = self.mix.patch_embed4
 
         # transformer encoder
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]  # stochastic depth decay rule
-        cur = 0
         self.block1 = nn.ModuleList([Block(self.mix.block1[i])
             for i in range(depths[0])])
         self.norm1 = self.mix.norm1
 
-        cur += depths[0]
         self.block2 = nn.ModuleList([Block(self.mix.block2[i])
             for i in range(depths[1])])
         self.norm2 = self.mix.norm2
 
-        cur += depths[1]
         self.block3 = nn.ModuleList([Block(self.mix.block3[i])
             for i in range(depths[2])])
         self.norm3 = self.mix.norm3
 
-        cur += depths[2]
         self.block4 = nn.ModuleList([Block(self.mix.block4[i])
             for i in range(depths[3])])
         self.norm4 = self.mix.norm4
