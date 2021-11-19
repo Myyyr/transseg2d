40790dd1e58423b41b8683a23fffe863ad208b27
MYR <loic.themyr@gmail.com>
2021-11-19 08:41:27 +0100

    sync

--- mmseg/models/backbones/mix_transformer_gt_omega.py
+++ mmseg/models/backbones/mix_transformer_gt_omega.py
@@ -292,24 +292,24 @@ class SegFormerGTOmega(nn.Module):
         self.window_size=(8,8)
 
 
-        self.global_token1 = torch.nn.Parameter(torch.randn(gt_num,embed_dims[0]))
+        self.global_token1 = torch.nn.Parameter(torch.randn(gt_num,self.embed_dims[0]))
         ws_pe = (40*gt_num//(2**0), 40*gt_num//(2**0))
-        self.pe1 = nn.Parameter(torch.zeros(gt_num, embed_dims[0]))
+        self.pe1 = nn.Parameter(torch.zeros(gt_num, self.embed_dims[0]))
         trunc_normal_(self.pe1, std=.02)
 
-        self.global_token2 = torch.nn.Parameter(torch.randn(gt_num,embed_dims[1]))
+        self.global_token2 = torch.nn.Parameter(torch.randn(gt_num,self.embed_dims[1]))
         ws_pe = (40*gt_num//(2**1), 40*gt_num//(2**1))
-        self.pe2 = nn.Parameter(torch.zeros(gt_num, embed_dims[1]))
+        self.pe2 = nn.Parameter(torch.zeros(gt_num, self.embed_dims[1]))
         trunc_normal_(self.pe2, std=.02)
 
-        self.global_token3 = torch.nn.Parameter(torch.randn(gt_num,embed_dims[2]))
+        self.global_token3 = torch.nn.Parameter(torch.randn(gt_num,self.embed_dims[2]))
         ws_pe = (40*gt_num//(2**2), 40*gt_num//(2**2))
-        self.pe3 = nn.Parameter(torch.zeros(gt_num, embed_dims[2]))
+        self.pe3 = nn.Parameter(torch.zeros(gt_num, self.embed_dims[2]))
         trunc_normal_(self.pe3, std=.02)
 
-        self.global_token4 = torch.nn.Parameter(torch.randn(gt_num,embed_dims[3]))
+        self.global_token4 = torch.nn.Parameter(torch.randn(gt_num,self.embed_dims[3]))
         ws_pe = (40*gt_num//(2**3), 40*gt_num//(2**3))
-        self.pe4 = nn.Parameter(torch.zeros(gt_num, embed_dims[3]))
+        self.pe4 = nn.Parameter(torch.zeros(gt_num, self.embed_dims[3]))
         trunc_normal_(self.pe4, std=.02)
 
 
