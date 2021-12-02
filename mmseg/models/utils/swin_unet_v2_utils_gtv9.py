16e14cf2c9deddc27fddce2deb59820b500dd14b
MYR <loic.themyr@gmail.com>
2021-12-02 18:51:16 +0100

    sync

--- mmseg/models/utils/swin_unet_v2_utils_gtv9.py
+++ mmseg/models/utils/swin_unet_v2_utils_gtv9.py
@@ -310,6 +310,7 @@ class SwinTransformerBlock(nn.Module):
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
+        self.do_gmsa = do_gmsa
         if do_gmsa:
             mlp_hidden_dim = int(mlp_ratio*dim)
 
