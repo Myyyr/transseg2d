742fa06eff03051ea098513f5c42513f251559d7
MYR <loic.themyr@gmail.com>
2021-12-02 18:32:12 +0100

    sync

--- mmseg/models/utils/swin_unet_v2_utils_gtv9.py
+++ mmseg/models/utils/swin_unet_v2_utils_gtv9.py
@@ -362,13 +362,14 @@ class SwinTransformerBlock(nn.Module):
         nHg, nWg = gt.shape[1], gt.shape[2]
         nHp, nWp = Hp//self.window_size, Wp//self.window_size
         if nHg != nHp or nWg != nWp:
+            ngt=gt.shape[3]
             gt = rearrange(gt, 'b h w g c -> (b g) c h w')
             nn.functional.interpolate(gt, size=(nHp, nWp), mode='bilinear')
-            gt = rearrange(gt, '(b g) c h w -> b h w g c', g=B)
+            gt = rearrange(gt, '(b g) c h w -> b h w g c', g=ngt)
         gt = rearrange(gt, 'b h w g c -> (b h w) g c')
-        print("\n\n\n\n")
-        print("x_windows", x_windows.shape)
-        print("gt", gt.shape)
+        # print("\n\n\n\n")
+        # print("x_windows", x_windows.shape)
+        # print("gt", gt.shape)
         skip_gt = gt
 
         # W-MSA/SW-MSA
