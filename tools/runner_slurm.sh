11a6d76c76c22c1f8569257f899d5c725b6a8330
MYR <loic.themyr@gmail.com>
2021-12-29 11:09:17 +0100

    pret

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -5,10 +5,11 @@
 #SBATCH --gres=gpu:4                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
-#SBATCH --time=00:05:59              # maximum execution time (HH:MM:SS)
-#SBATCH --qos=qos_gpu-dev
+#SBATCH --time=19:19:59              # maximum execution time (HH:MM:SS)
+#SBATCH --qos=qos_gpu-t3
 #SBATCH --output=logs/swg10ba%j.out # output file name
 #SBATCH --error=logs/swg10ba%j.err  # error file name
+#SBATCH -C v100-32g
 
 set -x
 
