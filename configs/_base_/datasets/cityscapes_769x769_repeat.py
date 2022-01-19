d1216f7f8ccfdb35fb010428a101abb915d58378
MYR <loic.themyr@gmail.com>
2022-01-19 11:29:34 +0100

    extended  - pos

--- configs/_base_/datasets/cityscapes_769x769_repeat.py
+++ configs/_base_/datasets/cityscapes_769x769_repeat.py
@@ -1,4 +1,6 @@
-_base_ = './cityscapes.py'
+# _base_ = './cityscapes.py'
+dataset_type = 'CityscapesDataset'
+data_root = 'data/cityscapes/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size = (769, 769)
