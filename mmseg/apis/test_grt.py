ed2174ac6f9457bbf8550b05ee7b3501189d861e
MYR <loic.themyr@gmail.com>
2022-05-25 15:18:07 +0200

    rebutal eccv

--- mmseg/apis/test_grt.py
+++ mmseg/apis/test_grt.py
@@ -62,7 +62,9 @@ def single_gpu_test_grt(model,
 
         if show or out_dir:
             img_tensor = data['img'][0]
-            result = data['gt_semantic_seg'][0]
+            result = data['gt_semantic_seg']
+            print(len(results))
+            print(len(results[0]))
             print(results.shape)
             exit(0)
             img_metas = data['img_metas'][0].data[0]
