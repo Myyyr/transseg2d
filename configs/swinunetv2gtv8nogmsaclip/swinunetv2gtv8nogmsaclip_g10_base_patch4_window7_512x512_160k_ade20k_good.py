62c0fc6d1d91f543f8b6b953a3f5811e5a84dec8
Myr <myr@pop-os.localdomain>
2022-10-06 13:52:11 +0200

    clip

--- configs/swinunetv2gtv8nogmsaclip/swinunetv2gtv8nogmsaclip_g10_base_patch4_window7_512x512_160k_ade20k_good.py
+++ configs/swinunetv2gtv8nogmsaclip/swinunetv2gtv8nogmsaclip_g10_base_patch4_window7_512x512_160k_ade20k_good.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/swin_unet_v2_gtv8_nogmsaclip.py', '../_base_/datasets/ade20k.py',
+    '../_base_/models/swin_unet_v2_gtv8_nogmsa_clip.py', '../_base_/datasets/ade20k.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
 ]
 model = dict(
