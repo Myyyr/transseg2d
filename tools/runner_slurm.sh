2c81c5985baa523a34d2116653ed8874a03c9f7e
Myr <myr@pop-os.localdomain>
2022-10-06 14:39:29 +0200

    clip

--- tools/runner_slurm.sh
+++ tools/runner_slurm.sh
@@ -5,8 +5,8 @@
 #SBATCH --gres=gpu:4                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
-#SBATCH --time=00:10:00             # maximum execution time (HH:MM:SS)
-#SBATCH --qos=qos_gpu-dev
+#SBATCH --time=99:00:00             # maximum execution time (HH:MM:SS)
+#SBATCH --qos=qos_gpu-t4
 #SBATCH --output=logs/sunnndclip.out # output file name # add %j to id the job
 #SBATCH --error=logs/sunnndclip.err  # error file name # add %j to id the job
 #SBATCH -C v100-32g
